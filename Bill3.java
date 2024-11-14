c96bbecf0689c1effe7073eb2e42d80b330f13f8
noa-halevi <noahalv123@gmail.com>
2024-11-14 19:40:04 +0200

    Update Bill3.java1

--- Bill3.java
+++ Bill3.java
@@ -8,7 +8,7 @@ public class Bill3 {
 		int total = Integer.parseInt(args[3]);
 
 		double bill = Math.ceil( total / 3.0 );
-		System.out.println("Dear " + name1 + ", " + name2 + ", and " + name3 + ": pay " + bill + " Shekels each.");
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + bill + " Shekels each.");
 	    
 	}
 }
